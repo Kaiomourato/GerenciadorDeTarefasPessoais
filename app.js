35d9a5b1196926e98ba6d22b08a37efefb1bf9c7
Kaio Mourato <kaiomouratodm@hotmail.com>
2025-08-11 19:56:10 -0300

    Resolvendo problema de conexão com o Supabase

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const { createClient } = require('@supabase/supabase-js');
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
